a7162a02b9d40fbbca1576ba5743eb8fd1907049
Muskan Mandil <muskan05mandil@gmail.com>
2024-07-16 17:48:32 +0530

    navbar & landing page responsivity done

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import streak_logo from "../../assets/streak_logo.svg";
@@ -9,10 +9,14 @@ import { ProgressContext } from "../../context/ProgressContext";
 import { AppContext } from "../../context/AppContext";
 import leaderboard from "../../assets/leaderboard.svg";
 import pointsxp from "../../assets/pointsxp.svg";
+import burger_menu from "../../assets/burger_menu.svg";
 
 const Navbar = () => {
-  const {filter} = useContext(AppContext);
-  const {progressInfo, levelQuestions, levelQuestionsDone} = useContext(ProgressContext);
+  const { filter } = useContext(AppContext);
+  const { progressInfo, levelQuestions, levelQuestionsDone } =
+    useContext(ProgressContext);
+
+  const [mobileMenu, setMobileMenu] = useState(false);
 
   let today = new Date(Date.now())
     .toLocaleString("en-IN", {
@@ -31,13 +35,19 @@ const Navbar = () => {
       {localStorage.getItem("auth-token") && (
         <div className="total-progress-container">
           <p className="total-progress-percentage">
-            {Math.ceil((levelQuestionsDone(filter)/ levelQuestions(filter)) * 100)}% completed
+            {Math.ceil(
+              (levelQuestionsDone(filter) / levelQuestions(filter)) * 100
+            )}
+            % completed
           </p>
-          <p className="total-progress">{levelQuestionsDone(filter)} / {levelQuestions(filter)}</p>
+          <p className="total-progress">
+            {levelQuestionsDone(filter)} / {levelQuestions(filter)}
+          </p>
         </div>
       )}
 
       <div className="navbar-right">
+        {/* {true ? ( //for testing purpose */}
         {localStorage.getItem("auth-token") ? (
           <>
             <div className="navbar-points">
@@ -67,6 +77,43 @@ const Navbar = () => {
             <Link to="/profile" className="navbar-profile">
               <img src={user_avatar} alt="" />
             </Link>
+            {mobileMenu ? (
+              <div className="mobile-navbar-expanded">
+                <div
+                  className="burger-menu"
+                  onClick={() => setMobileMenu(false)}
+                >
+                  <img src={burger_menu} alt="" className="burger-menu-logo" />
+                </div>
+                <div className="mobile-navbar-popup">
+                  <Link
+                    to="/leaderboard"
+                    className="navbar-leaderboard mobile-navbar-leaderboard"
+                    onClick={() => setMobileMenu(false)}
+                  >
+                    <img src={leaderboard} alt="" />
+                    <p>Leaderboard</p>
+                  </Link>
+                  <Link
+                    to="/profile"
+                    className="navbar-profile mobile-navbar-profile"
+                    onClick={() => setMobileMenu(false)}
+                  >
+                    <img src={user_avatar} alt="" />
+                    <p>Profile</p>
+                  </Link>
+                </div>
+              </div>
+            ) : (
+              <div className="mobile-navbar">
+                <div
+                  className="burger-menu"
+                  onClick={() => setMobileMenu(true)}
+                >
+                  <img src={burger_menu} alt="" className="burger-menu-logo" />
+                </div>
+              </div>
+            )}
           </>
         ) : (
           <Link to="/login">
